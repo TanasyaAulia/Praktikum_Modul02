90ebb8b6d6388b60f6731f011c0299a8a5f76aa6
Tanasya Aulia <Tanasyaaulia@gmail.com>
2021-06-14 09:12:13 +0800

    Format Tanggal dan Try Catch

--- src/Model/Mahasiswa.java
+++ src/Model/Mahasiswa.java
@@ -7,4 +7,6 @@ public class Mahasiswa {
     String nama;
     Date tanggalLahir;
 
+    }
+
 }
--- src/Model/MahasiswaBeraksi.java
+++ src/Model/MahasiswaBeraksi.java
@@ -1,25 +1,27 @@
 package Model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[]args){
+    public static void main(String[]args) {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19631105";
         mahasiswa.nama = "Tanasya";
-        mahasiswa.tanggalLahir = new Date();
+        try {
+            mahasiswa.tanggalLahir = sdf.parse("17-01-2001");
 
-        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
-
-        Mahasiswa mahasiswa2 = new Mahasiswa();
-        mahasiswa2.npm = "19635001";
-        mahasiswa2.nama = "Aulia";
-        mahasiswa2.tanggalLahir = new Date();
-
-        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+            System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+            System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+            System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa.tanggalLahir));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.err.println("Format tanggal tidak sesuai");
+        }
     }
 }
